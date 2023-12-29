fb0811aa0d53083d35a316a27b8ee84b42e404c7
Geert-Jan Knapen <gknapen@geertjanknapen.nl>
2023-12-29 12:18:45 +0100

    Add auth check on app startup.

--- HasToHappen/ViewModels/MainViewViewModel.swift
+++ HasToHappen/ViewModels/MainViewViewModel.swift
@@ -5,4 +5,23 @@
 //  Created by Geert-Jan Knapen on 27/12/2023.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewViewModel:ObservableObject {
+    @Published var currentUserId:String = ""
+    
+    private var handler: AuthStateDidChangeListenerHandle? // ? Means it is nil (null) by default
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
